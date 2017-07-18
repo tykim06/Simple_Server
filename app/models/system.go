12f1f6f60e1ededaf7b3fbf654e5ba50f2d90231
taeyeop kim <ktim610@gmail.com>
2017-07-18 09:10:47 +0900

    merge system systemJson

--- app/models/system.go
+++ app/models/system.go
@@ -23,3 +23,33 @@ func (c *System) PreInsert(_ gorp.SqlExecutor) error {
 func (c System) String() string {
 	return fmt.Sprintf("Systems(%d, %s, %s, %s, %s)", c.Id, c.Model, c.Health, c.Power, c.CreatedAt)
 }
+
+type SystemJson struct {
+	Bios struct {
+		Current struct {
+			VersionString string `json:"VersionString"`
+		} `json:"Current"`
+	} `json:"Bios"`
+	Model      string `json:"Model"`
+	Name       string `json:"Name"`
+	Power      string `json:"Power"`
+	Processors struct {
+		Count           int    `json:"Count"`
+		ProcessorFamily string `json:"ProcessorFamily"`
+		Status          struct {
+			HealthRollUp string `json:"HealthRollUp"`
+		} `json:"Status"`
+	} `json:"Processors"`
+	SKU          string `json:"SKU"`
+	SerialNumber string `json:"SerialNumber"`
+	Status       struct {
+		Health string `json:"Health"`
+		State  string `json:"State"`
+	} `json:"Status"`
+}
+
+func (c SystemJson) JsonToDB(system *System) {
+	system.Model = c.Model
+	system.Health = c.Processors.Status.HealthRollUp
+	system.Power = c.Power
+}
